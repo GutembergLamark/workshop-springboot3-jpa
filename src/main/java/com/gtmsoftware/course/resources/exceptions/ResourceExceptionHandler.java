a2742e0b3e6d4b93d11ea54291a818a39a10a8b4
Gutemberg Lamark <gtmbrglamark@gmail.com>
2025-02-15 16:20:02 -0300

    Exception handling - delete

--- src/main/java/com/gtmsoftware/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/gtmsoftware/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,24 +7,33 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.gtmsoftware.course.services.exceptions.DatabaseException;
 import com.gtmsoftware.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-	
+
 	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String typeError = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		
-		StandardError error = new StandardError(
-				Instant.now(), 
-				status.value(), 
-				"Resource not found", 
-				e.getMessage(), 
+
+		StandardError error = new StandardError(Instant.now(), status.value(), typeError, e.getMessage(),
 				request.getRequestURI());
-		
+
+		return ResponseEntity.status(status).body(error);
+	}
+
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> databseException(DatabaseException e, HttpServletRequest request) {
+		String typeError = "Database error";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+
+		StandardError error = new StandardError(Instant.now(), status.value(), typeError, e.getMessage(),
+				request.getRequestURI());
+
 		return ResponseEntity.status(status).body(error);
 	}
 }
